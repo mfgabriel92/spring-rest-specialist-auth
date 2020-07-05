f193dc024aae32a174f0604fea3c4172eeb53912
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-07-05 18:21:03 -0300

    22.9. Configuring the Authorization Server flow with Password Credentials and Opaque Tokens

--- src/main/java/br/gabriel/springrestspecialist/auth/AuthorizationServerConfig.java
+++ src/main/java/br/gabriel/springrestspecialist/auth/AuthorizationServerConfig.java
@@ -1,12 +1,36 @@
 package br.gabriel.springrestspecialist.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private PasswordEncoder encoder;
 
+    @Autowired
+    private AuthenticationManager manager;
+    
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+            .withClient("spring-rest-specialist-web")
+            .secret(encoder.encode("web"))
+            .authorizedGrantTypes("password")
+            .scopes("write", "read")
+            .accessTokenValiditySeconds(60 * 60 * 1);
+    }
+    
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(manager);
+    }
 }
